c7b57d0235b96f92fb152d7d016a5713fd7e4a4f
hab <Mohabzax4@gmail.com>
2022-12-25 23:54:24 +0200

    update

--- lib/Pages/Main/Categories_page.dart
+++ lib/Pages/Main/Categories_page.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:baby_care/Pages/Feeding/main_page.dart';
+import 'package:baby_care/Pages/Health/main_page.dart';
+import 'package:baby_care/Pages/Soothing/main_page.dart';
+import 'package:baby_care/Resources/assets_manager.dart';
 import 'package:baby_care/Resources/styles_manager.dart';
+import 'package:baby_care/Resources/widget_manager.dart';
 import 'package:flutter/material.dart';
 
 import '../../Resources/colors_manager.dart';
@@ -33,92 +37,74 @@ class Categories extends StatelessWidget {
                 children: [
                   GestureDetector(
                     onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => FeedingMain(),)),
-                    child: Container(
-                        height: 170,
-                        width: 175,
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: AppColors.primary),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey,
-                                blurRadius: 4,
-                                offset: Offset(1, 1),
-                              )
-                            ]),
-                        child: Column()),
+                    child: Cat(widget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.rotate(angle: 30.8,
+                        child: Image.asset(AppAssets.bottle,scale: 0.6,)),
+                        SizedBox(height: 5,),
+                        Text("Feeding",style: getMediumStyle(color: AppColors.grey,fontSize: 25),)
+                      ],
+                    ))
                   ),
-                  Container(
-                      height: 170,
-                      width: 175,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: AppColors.primary),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(1, 1),
-                            )
-                          ]),
-                      child: Column()),
+                  GestureDetector(
+                    onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => SoothingMain(),)),
+                    child: Cat(widget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.rotate(angle: 30.8,
+                        child: Image.asset(AppAssets.sleep,scale: 0.6,)),
+                        SizedBox(height: 5,),
+                        Text("Soothing",style: getMediumStyle(color: AppColors.grey,fontSize: 25),)
+                      ],
+                    ))
+                  ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                      height: 170,
-                      width: 175,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: AppColors.primary),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(1, 1),
-                            )
-                          ]),
-                      child: Column()),
-                  Container(
-                      height: 170,
-                      width: 175,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: AppColors.primary),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(1, 1),
-                            )
-                          ]),
-                      child: Column()),
+                  GestureDetector(
+                      onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => HealthMain(),)),
+                      child: Cat(widget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Transform.rotate(angle: 30.8,
+                              child: Image.asset(AppAssets.bottle,scale: 0.6,)),
+                          SizedBox(height: 5,),
+                          Text("Health",style: getMediumStyle(color: AppColors.grey,fontSize: 25),)
+                        ],
+                      ))
+                  ),
+                  GestureDetector(
+                      onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => FeedingMain(),)),
+                      child: Cat(widget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Transform.rotate(angle: 30.8,
+                              child: Image.asset(AppAssets.bottle,scale: 0.6,)),
+                          SizedBox(height: 5,),
+                          Text("Nappy",style: getMediumStyle(color: AppColors.grey,fontSize: 25),)
+                        ],
+                      ))
+                  ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                      height: 170,
-                      width: 175,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: AppColors.primary),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(1, 1),
-                            )
-                          ]),
-                      child: Column()),
+                  GestureDetector(
+                      onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => FeedingMain(),)),
+                      child: Cat(widget: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Transform.rotate(angle: 30.8,
+                              child: Image.asset(AppAssets.bottle,scale: 0.6,)),
+                          SizedBox(height: 5,),
+                          Text("Growth",style: getMediumStyle(color: AppColors.grey,fontSize: 25),)
+                        ],
+                      ))
+                  ),
                 ],
               ),
             ],
--- lib/Resources/widget_manager.dart
+++ lib/Resources/widget_manager.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:baby_care/Resources/styles_manager.dart';
@@ -6,7 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'colors_manager.dart';
 
-Widget Box({required String text}){
+Widget Box({required String text}) {
   return Container(
     height: 60,
     margin: EdgeInsets.symmetric(horizontal: 16),
@@ -16,36 +15,38 @@ Widget Box({required String text}){
       color: AppColors.lightGrey,
       borderRadius: BorderRadius.circular(10),
       boxShadow: [
-        BoxShadow(
-            color: AppColors.grey,
-            blurRadius: 4,
-            offset: Offset(1,3)
-        ),
+        BoxShadow(color: AppColors.grey, blurRadius: 4, offset: Offset(1, 3)),
       ],
     ),
     alignment: Alignment.centerLeft,
-    child: Text(text,style: getRegularStyle(color: AppColors.grey,fontSize: 15),),
+    child: Text(
+      text,
+      style: getRegularStyle(color: AppColors.grey, fontSize: 15),
+    ),
   );
 }
 
-Widget Button({required String text,Color? background,Color? textColor}){
+Widget Button({required String text, Color? background, Color? textColor}) {
   return Container(
     height: 50,
     margin: EdgeInsets.symmetric(horizontal: 16),
     padding: EdgeInsets.all(8),
     width: double.infinity,
     decoration: BoxDecoration(
-      color: background?? AppColors.primary,
+      color: background ?? AppColors.primary,
       borderRadius: BorderRadius.circular(10),
     ),
     alignment: Alignment.center,
-    child: Text(text,style: getRegularStyle(color: textColor?? AppColors.white,fontSize: 20),),
+    child: Text(
+      text,
+      style: getRegularStyle(color: textColor ?? AppColors.white, fontSize: 20),
+    ),
   );
 }
 
-Widget StateCard({required Widget widget}){
+Widget StateCard({required Widget widget}) {
   return Container(
-    padding: EdgeInsets.only(left: 16,top: 16,bottom: 8,right: 8),
+    padding: EdgeInsets.only(left: 16, top: 16, bottom: 8, right: 8),
     height: 150,
     width: 170,
     decoration: BoxDecoration(
@@ -55,23 +56,40 @@ Widget StateCard({required Widget widget}){
           BoxShadow(
             color: Colors.grey,
             blurRadius: 4,
-            offset: Offset(1,3.5),
+            offset: Offset(1, 3.5),
           )
-        ]
-    ),
+        ]),
     child: widget,
   );
 }
 
-Widget RecentCard({required Widget widget,Color? background}){
-  return  Container(
+Widget RecentCard({required Widget widget, Color? background}) {
+  return Container(
     height: 80,
     width: double.infinity,
     padding: EdgeInsets.all(16),
     decoration: BoxDecoration(
-        color: background?? Colors.white,
-        borderRadius: BorderRadius.circular(25)
-    ),
+        color: background ?? Colors.white,
+        borderRadius: BorderRadius.circular(25)),
     child: widget,
   );
+}
+
+Widget Cat({required Widget widget}) {
+  return Container(
+      height: 170,
+      width: 175,
+      decoration: BoxDecoration(
+        color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: AppColors.primary),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 4,
+              offset: Offset(1, 1),
+            )
+          ]),
+      alignment: Alignment.center,
+      child: widget);
 }
